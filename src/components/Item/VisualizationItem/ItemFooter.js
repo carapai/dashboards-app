ddbe05543da73545b29a2e58efc4ad8a41e88cc5
joakimia <joakimamundsen91@gmail.com>
2018-12-18 16:34:47 +0100

    pull latest from master, fix merge conflicts

--- src/components/Item/VisualizationItem/ItemFooter.js
+++ src/components/Item/VisualizationItem/ItemFooter.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import InterpretationsComponent from '@dhis2/d2-ui-interpretations';
 import { colors } from '../../../modules/colors';
-import { getId, getDescription } from './plugin';
-import Interpretations from './Interpretations/Interpretations';
+import { getId } from './plugin';
 
 const style = {
     scrollContainer: {
         overflowY: 'auto',
         height: '370px',
-        marginTop: '5px',
-        marginBottom: '5px',
-        paddingLeft: '14px',
     },
     line: {
         margin: '-1px 0px 0px',
@@ -34,34 +31,31 @@ const style = {
     },
 };
 
-const ItemDescription = ({ description }) => {
-    return (
-        <div style={style.descriptionContainer}>
-            <h3 style={style.descriptionTitle}>Description</h3>
-            <p style={style.descriptionText}>{description}</p>
-        </div>
-    );
-};
-
 class ItemFooter extends Component {
     render() {
         const objectId = getId(this.props.item);
-        const description = getDescription(this.props.item);
 
         return (
             <div className="dashboard-item-footer">
                 <hr style={style.line} />
                 <div style={style.scrollContainer}>
-                    <ItemDescription description={description} />
-                    <hr style={style.line} />
-                    <Interpretations
-                        object={this.props.item}
-                        objectId={objectId}
+                    <InterpretationsComponent
+                        d2={this.context.d2}
+                        type={this.props.item.type.toLowerCase()}
+                        id={objectId}
                     />
                 </div>
             </div>
         );
     }
 }
+
+ItemFooter.contextTypes = {
+    d2: PropTypes.object.isRequired,
+};
+
+ItemFooter.propTypes = {
+    item: PropTypes.object.isRequired,
+};
 
 export default ItemFooter;
